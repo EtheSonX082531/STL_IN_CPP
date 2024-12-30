f21de77960ce6a739d5f46e45131db091bcd3577
Anik Roy <136085433+EtheSonX082531@users.noreply.github.com>
2024-12-30 17:32:40 +0600

    Create Enhanced for loop.cpp

---
+++ Enhanced for loop.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int>v= {1,2,3,4,5};
+    //Enhanced for loop
+    //For-each loop
+    //Rnage Based for loop
+    for(int i:v) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    char ch[4]= {'A','N','I','K'};
+    for(char i:ch) {
+        cout<<i<<" ";
+    }
+    return 0;
+}
